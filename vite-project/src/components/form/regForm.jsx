3af809cca7f87206e9bbfd2d44a8942138418b5e
Miami <alex.miami.petrov@gmail.com>
2025-04-02 23:01:10 +0300

    fix_form

--- vite-project/src/components/form/regForm.jsx
+++ vite-project/src/components/form/regForm.jsx
@@ -21,96 +21,102 @@ const RegForm = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   return (
-    <Container>
-      <Formik
-        initialValues={{ name: "", email: "", phone: "", agreement: false }}
-        validationSchema={validationSchema}
-        onSubmit={async (values) => {
-          setIsLoading(true);
-          try {
-            const responseData = await registerUser(values);
-            console.log("Форма успішно відправлена!", responseData);
-            iziToast.success({
-              title: "Успіх",
-              message: "Форма успішно відправлена!",
-            });
-          } catch (error) {
-            console.error("Помилка відправлення форми:", error);
-            iziToast.error({
-              title: "Помилка",
-              message: "Помилка відправлення форми!",
-            });
-          } finally {
-            setIsLoading(false);
-          }
-        }}
-      >
-        {({ errors, touched }) => (
-          <Form>
-            {isLoading && <div className={s.loading}>Завантаження...</div>}
+    <>
+      <Container>
+        <Formik
+          initialValues={{ name: "", email: "", phone: "", agreement: false }}
+          validationSchema={validationSchema}
+          onSubmit={async (values) => {
+            setIsLoading(true);
+            try {
+              const responseData = await registerUser(values);
+              console.log("Форма успішно відправлена!", responseData);
+              iziToast.success({
+                title: "Успіх",
+                message: "Форма успішно відправлена!",
+              });
+            } catch (error) {
+              console.error("Помилка відправлення форми:", error);
+              iziToast.error({
+                title: "Помилка",
+                message: "Помилка відправлення форми!",
+              });
+            } finally {
+              setIsLoading(false);
+            }
+          }}
+        >
+          {({ errors, touched }) => (
+            <Form className={s.form}>
+              {isLoading && <div className={s.loading}>Завантаження...</div>}
 
-            <Field
-              name="name"
-              placeholder="Введіть своє ім'я"
-              className={s.input}
-            />
-            <ErrorMessage name="name" component="div" className={s.error} />
-
-            <Field
-              name="email"
-              type="email"
-              placeholder="Введіть свій E-mail"
-              className={s.input}
-            />
-            <ErrorMessage name="email" component="div" className={s.error} />
-            <div className={s.phoneInputWrapper}>
-              <img
-                className={s.flagIcon}
-                src={flag}
-                alt="flag"
-                width="24"
-                height="16"
-              />
               <Field
-                name="phone"
-                placeholder="+380 95 -- --- --"
+                name="name"
+                placeholder="Введіть своє ім'я"
                 className={s.input}
               />
-            </div>
-            <ErrorMessage name="phone" component="div" className={s.error} />
+              <ErrorMessage name="name" component="div" className={s.error} />
 
-            <label className={s.agreementLabel}>
-              <Field type="checkbox" name="agreement" className={s.checkbox} />
-              <div className={s.checkWrap}>
-                <p className={s.checkText}>
-                  Згоден з
-                  <span className={s.span}>
-                    <a href="#" className={s.link}>
-                      Політикою конфіденційності
-                    </a>
-                  </span>
-                  та
-                  <span className={s.span}>
-                    <a href="#" className={s.link}>
-                      Умовами користування послугами
-                    </a>
-                  </span>
-                </p>
+              <Field
+                name="email"
+                type="email"
+                placeholder="Введіть свій E-mail"
+                className={s.input}
+              />
+              <ErrorMessage name="email" component="div" className={s.error} />
+              <div className={s.phoneInputWrapper}>
+                <img
+                  className={s.flagIcon}
+                  src={flag}
+                  alt="flag"
+                  width="24"
+                  height="16"
+                />
+                <Field
+                  name="phone"
+                  placeholder="+380 95 -- --- --"
+                  className={s.input}
+                />
               </div>
-            </label>
-            <ErrorMessage
-              name="agreement"
-              component="div"
-              className={s.error}
-            />
+              <ErrorMessage name="phone" component="div" className={s.error} />
 
-            <button type="submit" className={s.regBut} disabled={isLoading}>
-              {isLoading ? "Відправлення..." : "Зареєструватися"}
-            </button>
-          </Form>
-        )}
-      </Formik>
-    </Container>
+              <label className={s.agreementLabel}>
+                <Field
+                  type="checkbox"
+                  name="agreement"
+                  className={s.checkbox}
+                />
+                <div className={s.checkWrap}>
+                  <p className={s.checkText}>
+                    Згоден з
+                    <span className={s.span}>
+                      <a href="#" className={s.link}>
+                        Політикою конфіденційності
+                      </a>
+                    </span>
+                    та
+                    <span className={s.span}>
+                      <a href="#" className={s.link}>
+                        Умовами користування послугами
+                      </a>
+                    </span>
+                  </p>
+                </div>
+              </label>
+              <ErrorMessage
+                name="agreement"
+                component="div"
+                className={s.error}
+              />
+
+              <button type="submit" className={s.regBut} disabled={isLoading}>
+                {isLoading ? "Відправлення..." : "Зареєструватися"}
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </Container>
+    </>
   );
 };
 
